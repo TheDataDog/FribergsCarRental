fb2a46a18ec234413c325288be9909307fcb21fc
Emelie <empa_petersson@hotmail.com>
2025-01-27 15:18:40 +0100

    Updated Bookingconfirmation

--- FribergsCarRental/Models/Adress.cs
+++ FribergsCarRental/Models/Adress.cs
@@ -6,12 +6,16 @@ namespace FribergsCarRental.Models
     {
         public int AdressId { get; set; }
         [Display(Name = "Gatuadress")]
+        [Required(ErrorMessage = "Gatuadress är obligatoriskt")]
         public string Street { get; set; } = "";
         [Display(Name = "Gatunummer")]
+        [Required(ErrorMessage = "Gatunummer är obligatoriskt")]
         public string StreetNumber { get; set; } = "";
         [Display(Name = "Postnummer")]
+        [Required(ErrorMessage = "Postnummer är obligatoriskt")]
         public string ZipCode { get; set; } = "";
         [Display(Name = "Stad")]
+        [Required(ErrorMessage = "Stad är obligatoriskt")]
         public string City { get; set; } = "";
     }
 }
