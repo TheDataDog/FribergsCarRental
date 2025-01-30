cc33d86d6d73e84dd43a3c2593b577905429195a
Emelie <empa_petersson@hotmail.com>
2025-01-30 09:46:19 +0100

    Migration success

---
+++ FribergsCarRental/Migrations/20250130084354_Forth.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FribergsCarRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class Forth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FuelConsumption",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "FuelConsumption",
+                table: "Cars",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
