046c7207eca5035fcb5b2b3ca619982a8a52aa45
Emelie <empa_petersson@hotmail.com>
2025-01-30 10:27:12 +0100

    Added CreatedCustomerViewModel

---
+++ FribergsCarRental/ViewModels/CustomerCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FribergsCarRental.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace FribergsCarRental.ViewModels
+{
+    public class CustomerCreateViewModel
+    {
+        public Customer Customer { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
